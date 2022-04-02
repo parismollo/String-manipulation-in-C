646398ed346d99992ae51bcc7964ca78cfd3a1c8
Paris Mollo <parismollochristondis@gmail.com>
2022-04-02 21:28:48 +0200

    exercice 3.1 completed

--- tp6.c
+++ tp6.c
@@ -3,6 +3,11 @@
 #include <ctype.h>
 #include <string.h>
 
+typedef struct {
+    size_t indice; 
+    size_t len;
+} mutation;
+
 char* dupliquer(const char* s) {
     int size = strlen(s);
     char* dest = malloc(size+1);
@@ -49,6 +54,31 @@ char* multiplier(const char* s, unsigned int a) {
     return res;
 }
 
+int nboc(const char *s, const char *sub) {
+    /*
+    Attention: 
+    - Fonction inversé par rapport le tp
+    Cherche le nombre d'occurrences de s dans sub!!!!
+    */
+    int nb_occurences = 0;
+    int len_s = strlen(s), len_sub = strlen(sub);
+    int idx_s = 0;
+    
+    if(len_s > len_sub) return nb_occurences;
+    for(int i = 0; i < len_sub; i++) {
+        if(sub[i] == s[idx_s]) {
+            idx_s++;
+            if(idx_s == len_s) {
+                nb_occurences++;
+                idx_s = 0;
+            }
+        }else {
+            idx_s = 0;
+        }
+    }
+    return nb_occurences;
+}
+
 void tests_1() {
     printf("[LOG]: duplicate():  %s\n", dupliquer("Hello World"));
     printf("[LOG]: duplicate2(): %s\n", dupliquer2("Hello World"));
@@ -74,5 +104,8 @@ int main(int argc, char* argv[]) {
         int s = atoi(argv[2]);
         printf("%s", multiplier(argv[1], s));
     }
+
+    printf("nbocc: %d", nboc("aa", "dsdaaaaddsaa"));
+
     return EXIT_SUCCESS;
 }
